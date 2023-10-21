c4db25ed244261288c99cc8e94ef8373c3ce9c06
Camille SIESSE CUSCUSA <camille.siessecuscusa@gmail.com>
2023-10-21 15:39:03 +0200

    routes correction

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <nav>
+      <Link to="/">Accueil</Link>
+      <Link to="/apropos"> A propos</Link>
+      <Link to="/fichelogement"> Fiche logement</Link>
+    </nav>
+  );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style/index.css";
-import App from "./components/App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Apropos from "./pages/Apropos/";
+import Header from "./components/Header";
+import Error from "./pages/Error";
+import Fichelogement from "./pages/Fichelogement";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apropos" element={<Apropos />} />
+        <Route path="/fichelogement" element={<Fichelogement />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
