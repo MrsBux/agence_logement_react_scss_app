bf24a9d19d02562bc72b212d96c9802384a6d3f4
Camille SIESSE CUSCUSA <camille.siessecuscusa@gmail.com>
2023-10-21 16:11:35 +0200

    création modification du composant header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "../../style/header.css";
 
 function Header() {
   return (
-    <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/apropos"> A propos</Link>
-      <Link to="/fichelogement"> Fiche logement</Link>
-    </nav>
+    <header>
+      <div className="App">
+        <img src={logo} alt="logo kasa"></img>
+      </div>
+
+      <nav>
+        <Link to="/">Accueil</Link>
+        <Link to="/apropos"> A propos</Link>
+        {/* <Link to="/fichelogement"> Fiche logement</Link> */}
+      </nav>
+    </header>
   );
 }
 
