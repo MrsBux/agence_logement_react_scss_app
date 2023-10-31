c770d159ed54fe8db3dc7078900de01a7cf1b684
Camille SIESSE CUSCUSA <camille.siessecuscusa@gmail.com>
2023-10-31 11:12:49 +0100

    erreur.s Id sur une propriété gérée.s avec <Navigate>

--- src/pages/Fichelogement/index.jsx
+++ src/pages/Fichelogement/index.jsx
@@ -2,13 +2,14 @@ import { useParams } from "react-router-dom";
 import Carrousel from "../../components/Carrousel";
 import Informations from "../../components/Informations";
 import datas from "../../datas/data.json";
+import { Navigate } from "react-router-dom";
 
 function Fichelogement() {
   const { id } = useParams();
   const logement = datas.find((logement) => logement.id === id);
 
   if (!logement) {
-    return <div>Logement non trouvé.</div>;
+    return <Navigate to="*" replace={true} />;
   }
   return (
     <div className="section_logement">
